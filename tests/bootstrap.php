a608a7c0f2f753e60c347ac0c129f50d8d1e4dcc
Bas de Ruiter <bas@harver.com>
2016-12-08 16:52:05 +0100

    throw-away commit

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,3 +1,20 @@
 <?php declare(strict_types = 1);
 
 require_once __DIR__ . '/../vendor/autoload.php';
+
+define('CONFIG_PATH', __DIR__ . '/../config');
+define('TESTS_PATH', __DIR__);
+
+putenv('DB_CONNECTION=sqlite');
+putenv('DB_DATABASE=' . TESTS_PATH . '/testdb.sqlite');
+
+function reset_test_db()
+{
+    chdir(TESTS_PATH);
+    copy('stubs/stub-testdb.sqlite', 'testdb.sqlite');
+    chdir(TESTS_PATH . '/..');
+}
+reset_test_db();
+
+// boot Laravel
+require_once __DIR__ . '/../bootstrap/autoload.php';
